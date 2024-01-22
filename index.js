c5c97eefe6b4ec657f9d28828d17541cbf0326f0
Raktim Chowdhury <136430004+Raktimch84@users.noreply.github.com>
2024-01-23 01:25:46 +0530

    Update index.js
    changed folder heirarchy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
 import reportWebVitals from './reportWebVitals';
-import Morningproject from './components/morningproject';
+import Morningproject from './morningproject';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
