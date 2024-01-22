49db1dedbb2af4949d07c0acb9b85ac2e2e7dc3f
Raktim Chowdhury <136430004+Raktimch84@users.noreply.github.com>
2024-01-23 01:24:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.css';
+import reportWebVitals from './reportWebVitals';
+import Morningproject from './components/morningproject';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <Morningproject />
+  </>
+);
+reportWebVitals();
